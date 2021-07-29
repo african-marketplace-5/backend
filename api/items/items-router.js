eacc5c35622f8c5c60721c14944d5b066c2021bc
kazden.jolley <kazden.jolley@gmail.com>
2021-07-29 08:23:57 -0600

    added restricted middleware to all endpoints

--- api/items/items-router.js
+++ api/items/items-router.js
@@ -1,15 +1,15 @@
 const express = require('express');
 
-const { restricted } = require('../auth/auth-middleware'); // eslint-disable-line
+const { restricted } = require('../auth/auth-middleware');
 const Item = require('./items-model');
 
 const router = express.Router();
 
-router.get('/', /*restricted,*/ async (req, res) => {
+router.get('/', restricted, async (req, res) => {
   res.json(await Item.getAllItems())
 })
 
-router.post('/', /*restricted,*/ async (req, res) => {
+router.post('/', restricted, async (req, res) => {
   Item.add(req.body)
     .then(newItem => {
       res.status(201).json(newItem);
@@ -19,7 +19,7 @@ router.post('/', /*restricted,*/ async (req, res) => {
     })
 })
 
-router.get('/filter/:category_id', /*restricted,*/ (req, res) => {
+router.get('/filter/:category_id', restricted, (req, res) => {
   const { category_id } = req.params;
 
   Item.getByCategoryId(category_id)
